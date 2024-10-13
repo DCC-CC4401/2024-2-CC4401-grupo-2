2486eef5e7f0528cfa98ed72edf81ae40e076659
KleinMetallicis <joaxz1@gmail.com>
2024-10-13 20:27:00 -0300

    ADD: restaurantes.html

--- todoapp/admin.py
+++ todoapp/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
-from todoapp.models import Categoria
-from todoapp.models import User, Tarea
+from todoapp.models import User, Tarea, Restaurant
 from categorias.models import Categoria
  
 admin.site.register(Categoria)
 admin.site.register(User)
 admin.site.register(Tarea)
+admin.site.register(Restaurant)
