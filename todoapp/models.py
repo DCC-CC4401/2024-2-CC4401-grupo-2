daaf1fb5732002b956608ad009c26ee8b948735f
Damian-Gonzalez-G <damian.gonzalez@ug.uchile.cl>
2024-10-14 20:28:32 -0300

    Added model Review in models.py that will represent a review given by a user to a determined restaurant

--- todoapp/models.py
+++ todoapp/models.py
@@ -49,5 +49,25 @@ Características añadidas:
 class User(AbstractUser):
     pronombres = [('La','La'),('El','El'),('Le','Le'),('Otro','Otro')]
     pronombre = models.CharField(max_length=5,choices=pronombres)
-    
 
+
+"""
+Modelo para representar una review de un restaurante
+de nuestra aplcación web
+
+- restaurant: Nombre del restaurante.
+- user: Usuario que dejó la review.
+- comment: Contenido de la review.
+- rating: Puntuación del restaurante de 1 a 5 estrellas.
+- created_at: Cuando fue creada la review.
+"""  
+    
+class Review(models.Model):
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE, related_name="reviews")
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    comment = models.TextField()
+    rating = models.IntegerField(choices=[(i, str(i)) for i in range(1, 6)])  # Rating entre 1 y 5 estrellas
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.restaurant.name} - {self.rating} estrellas"
