1ba9dc8b1faa797680ce0313ef6adaf275bfb801
Damian-Gonzalez-G <damian.gonzalez@ug.uchile.cl>
2024-11-26 18:27:37 -0300

    refactor: Changed the names of the boxes of the form when editing reviews and adding restaurants

--- todoapp/forms.py
+++ todoapp/forms.py
@@ -6,8 +6,21 @@ class RestaurantForm(forms.ModelForm):
         model = Restaurant
         fields = ['name', 'address', 'phone', 'email', 'website', 'description', 'categorias', 'comuna']
         exclude = ["lon", "lat"]
+        labels = {
+            'name': 'Nombre del Restaurante',
+            'address': 'Dirección',
+            'phone': 'Teléfono',
+            'email': 'Correo Electrónico',
+            'website': 'Sitio Web',
+            'description': 'Descripción',
+            'categorias': 'Categorías',
+        }
 
 class ReviewForm(forms.ModelForm):
     class Meta:
         model = Review
         fields = ['comment', 'rating']
+        labels = {
+            'rating': 'Puntuación',
+            'comment': 'Comentario',
+        }
